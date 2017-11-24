5b7a2eb6374654fd7c4daf9e1a83c052a5cd4d25
KRdimov <milush_karadimov@yahoo.com>
2017-11-24 23:36:23 +0200

    Handle CORS Policy issue with ajax requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,12 @@ require "error_handler"
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :add_cors_response_header
+
+  def add_cors_response_header 
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET'
+  end
+
   include ErrorHandler
 end
